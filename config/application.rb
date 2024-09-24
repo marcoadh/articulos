b918fa82ccb5ae80bc6ccbdd59ca32c63e4938cf
Marco De la cruz <marco@gmail.com>
2024-09-24 01:52:54 -0500

    Agregar default timezone

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Articulos
 
     config.i18n.default_locale = :es
 
+    config.time_zone = 'Lima'
+    config.active_record.default_timezone = :local
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
