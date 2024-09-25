e024999359fed7c48a8bbf514498574b258cd203
Marco De la cruz <marco@gmail.com>
2024-09-24 22:35:44 -0500

    Editar artículo, y solución de bug de show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
+
   def index
     @articles = Article.order(created_at: :desc).with_rich_text_content_and_embeds 
   end
@@ -10,22 +12,50 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+  end
+
   def create
     @article = Article.new(article_params)
 
     respond_to do |format|
       if @article.save
-        format.turbo_stream { render turbo_stream: turbo_stream.prepend('articles', partial: 'articles/article', locals: { article: @article }) }
-        format.html { redirect_to articles_path, notice: 'Artículo fue creado exitosamente.' }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.prepend('articles', partial: 'articles/article', locals: { article: @article })
+        end
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html do
+          render :new, status: :unprocessable_entity
+        end
       end
     end
+  end
+
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@article, partial: 'articles/article', locals: { article: @article })
+        end
+      else
+        format.html do
+          render :edit, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  def destroy
+
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
